1867ec0595e6fbaefb2a64300eab04670c9cea82
fernanda <fer.r.benetti@gmail.com>
2025-04-27 14:42:17 -0300

    fazendo tela de edição

--- frontend/vite-project/src/pages/EdicaoLivro/EdicaoLivro.jsx
+++ frontend/vite-project/src/pages/EdicaoLivro/EdicaoLivro.jsx
@@ -1,15 +1,45 @@
 import React from "react"
 import './EdicaoLivro.css'
+import { IconBook } from '@tabler/icons-react';
 
 
-const EdicaoLivro = () =>  {
-    return (
-      <div>
-        <div id="main">
+
+const EdicaoLivro = () => {
+  return (
+    <div>
+      <div id="main">
+
+        <div className="barra-pesquisa-container">
+          <input id="barrapesquisa" type="text" placeholder="Pesquise aqui o livro que você deseja editar" />
+          <button><IconBook></IconBook></button>
+        </div>
+
+        <div id="classificacao">
+          <p id="bk">Livro</p>
+          <p id="edit">Editar</p>
+          <p id="del">Excluir</p>
 
         </div>
+
+        <div id="listaLivros">
+          <div className="item1">
+            <img src="" alt="" />
+            <div id="informacoes">
+              <h5>titulo</h5>
+              <p>dados</p>
+            </div>
+            <div id="botoes">
+              <button>Editar</button>
+              <button>Excluir</button>
+
+            </div>
+          </div>
+        </div>
+
+
       </div>
-    );
+    </div>
+  );
 }
-  
+
 export default EdicaoLivro;
