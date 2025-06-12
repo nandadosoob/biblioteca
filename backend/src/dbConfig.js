a2192a765f0953fca1384564d2a8744fd3b13fea
beatriz <128046453+abeazist@users.noreply.github.com>
2025-06-11 23:19:11 -0300

    Update dbConfig.js

--- backend/src/dbConfig.js
+++ backend/src/dbConfig.js
@@ -3,8 +3,8 @@ const pg = require("pg");
 const pgPool = new pg.Pool({
     host: '127.0.0.1',
     database: 'livraria',
-    user: 'postgres',
-    password: 'postgres',
+    user: '-', //muda dps plmDs
+    password: '-',
     port: 5432,
     ssl: false
 });
