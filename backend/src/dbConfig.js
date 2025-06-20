6de492c6ca3073b8347cf49347e897a42883ac9c
fernanda <fer.r.benetti@gmail.com>
2025-06-20 15:47:53 -0300

    esqueci de mudar a senha do dbconfigkk

--- backend/src/dbConfig.js
+++ backend/src/dbConfig.js
@@ -3,8 +3,8 @@ const pg = require("pg");
 const pgPool = new pg.Pool({
     host: '127.0.0.1',
     database: 'livraria', //é case sensitive
-    user: 'postgres', //muda dps plmDs
-    password: 'fernandabebe',
+    user: '-', //muda dps plmDs
+    password: '-',
     port: 5432,
     ssl: false
 });
