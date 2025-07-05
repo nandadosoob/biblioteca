3a0a82bb21c376f913c9630c15a0aa9dc9f26e3f
fernanda <fer.r.benetti@gmail.com>
2025-07-05 20:33:18 -0300

    crud reserva e divida backend feitos

---
+++ backend/src/controllers/Divida.js
@@ -0,0 +1,79 @@
+const modelDivida = require('../models/Divida');
+
+
+async function create(req, res) {
+    const {id_locatario, id_livro, data_reserva, estado, valor, data_divida} = req.body;
+
+    if (!id_locatario || !id_livro) {
+        res.status(400).json({ error: 'Obrigatorio: Nome do locatario, campo nome_locatario' });
+        return;
+    }
+
+    try {
+        await modelDivida.create(id_locatario, id_livro, data_reserva, estado, valor, data_divida);
+        res.status(201).json({ message: 'ok' });
+    } catch (error) {
+        console.error('erro na função create do locatario', error.message);
+        res.status(500).json({ error: 'Erro ao criar locatario' });
+    }
+}
+
+async function list(req, res){
+    try{
+        let dividas = await modelDivida.list();
+        res.status(200).json({dividas:dividas});
+        return;
+    } catch( error ){
+        res.status(500).json({error: 'Erro no list de dividas'});
+        return;
+    }
+};
+
+async function get(req, res) {
+    const id_divida = req.params.id_divida;
+    try {
+        const divida = await modelDivida.get(id_divida);
+        if (!divida) {
+            res.status(404).json({ error: 'dividao não encontrada' });
+            return;
+        }
+        res.status(200).json({ divida });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar divida' });
+    }
+}
+
+async function update(req, res) {
+  const id_divida = req.params.id_divida;
+  const { id_locatario, id_livro, data_reserva, estado, valor, data_divida} = req.body;
+
+  if (!id_locatario || !id_livro) {
+    return res.status(400).json({ error: 'Nome do locatario obrigatório' });
+  }
+
+  try {
+    const updatedCount = await modelDivida.update(id_divida, id_locatario, id_livro, data_reserva, estado, valor, data_divida);
+
+    if (updatedCount === 1) {
+      return res.status(200).json({ message: 'divida atualizada com sucesso' });
+    } else {
+      return res.status(404).json({ error: 'divida não encontrada' });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: 'Erro ao atualizar divida' });
+  }
+}
+
+async function remove(req, res) {
+    const id_divida = req.params.id_divida; 
+    try {
+        await modelDivida.remove(id_divida);
+        res.status(204).send(); 
+    } catch (error) {
+        console.error('Erro ao remover divida:', error.message);
+        res.status(500).json({ error: 'Erro ao remover divida' });
+    }
+}
+
+
+module.exports = { create, list, get, update, remove };
--- backend/src/models/Reserva.js
+++ backend/src/models/Reserva.js
@@ -11,3 +11,73 @@
 //     // passar o id do livro e quantas reservas tem ativa dele
 //     // dos 5 de machado de assis quantos estão reservados?
 // }
+
+const dbConfig = require('../dbConfig');
+const pgPool = dbConfig.pgPool;
+
+//MUDAR OS ATRIBUTOS PRA LOCATARIO
+
+// Função responsável por criar as reservas
+async function create(id_livro, id_locatario, data_reserva, data_retorno){
+    const query = "INSERT INTO reserva (id_livro, id_locatario, data_reserva, data_retorno) VALUES ($1, $2, $3, $4)"
+    let values = [id_livro, id_locatario, data_reserva, data_retorno];
+    try{
+        await pgPool.query(query, values);
+        return;
+    }catch(error){
+        console.log('Erro no create no model de reserva', error.message);
+        throw error;
+    }
+}
+
+// Função responsável por listar todas as reservas
+async function list(){
+    const query = "SELECT * FROM reserva";
+    try {
+        let reserva = await pgPool.query(query);
+        return reserva.rows;
+    } catch(error) {
+        console.error('Erro no list do reserva:', error.message); 
+        throw error;
+    }
+}
+
+// Função responsável por listar reservas
+async function get(id_livro, id_locatario, data_reserva){
+    const query = "SELECT * FROM reserva WHERE id_livro = $1 AND id_locatario = $2 AND Data_reserva = $3";
+    try {
+        let resultado = await pgPool.query(query, [id_livro, id_locatario, data_reserva]);
+        return resultado.rows[0]; // Retorna um único autor
+    } catch(error) {
+        console.error('Erro no get do divida:', error.message); 
+        throw error;
+    }
+}
+
+// Função responsável por atualizar uma reserva
+async function update(id_livro, id_locatario, data_reserva, data_retorno) {
+    const query = "UPDATE reserva SET data_retorno = $4 WHERE id_livro = $1 AND id_locatario = $2 AND Data_reserva = $3";
+    try {
+        const result = await pgPool.query(query, [id_livro, id_locatario, data_reserva, data_retorno]);
+        return result.rowCount; 
+    } catch (error) {
+        console.error('Erro no update do divida:', error.message);
+        throw error;
+    }
+
+}
+
+
+// Função responsável por deletar todos os locatários
+async function remove(id_livro, id_locatario, data_reserva) {
+    const query = "DELETE FROM reserva WHERE id_livro = $1 AND id_locatario = $2 AND Data_reserva = $3";
+    try {
+        await pgPool.query(query, [id_livro, id_locatario, data_reserva]);
+    } catch (error) {
+        console.error('Erro no remove do divida:', error.message);
+        throw error;
+    }
+}
+
+
+module.exports = {create:create, list:list, get:get, update:update, remove:remove};
