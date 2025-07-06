94b2cf251e8449141fe6ebb8f3795a6e57297ae0
Nix <130068039+covennix@users.noreply.github.com>
2025-07-06 00:20:02 -0300

    crud curso rodando

--- backend/src/dbConfig.js
+++ backend/src/dbConfig.js
@@ -2,10 +2,10 @@ const pg = require("pg");
 
 const pgPool = new pg.Pool({
     host: '127.0.0.1',
-    database: 'Livraria', //é case sensitive
+    database: 'livraria', //é case sensitive
     user: 'postgres', //muda dps plmDs
 
-    password: 'fernandabebe',
+    password: '01062006',
 //>>>>>>> c6da8ae6788c715be2d282e448f47ddcedc9cb81
     port: 5432,
     ssl: false
--- backend/src/models/Curso.js
+++ backend/src/models/Curso.js
@@ -2,7 +2,7 @@ const dbConfig = require('../dbConfig');
 const pgPool = dbConfig.pgPool;
 
 async function create(nome_curso) {
-  const query = "INSERT INTO cursos (nome_curso) VALUES ($1)";
+  const query = "INSERT INTO curso (nome_curso) VALUES ($1)";
   const values = [nome_curso];
   try {
     await pgPool.query(query, values);
@@ -14,7 +14,7 @@ async function create(nome_curso) {
 }
 
 async function list() {
-  const query = "SELECT * FROM cursos ORDER BY id_curso";
+  const query = "SELECT * FROM curso ORDER BY id_curso";
   try {
     const result = await pgPool.query(query);
     return result.rows;
@@ -25,7 +25,7 @@ async function list() {
 }
 
 async function get(id_curso) {
-  const query = "SELECT * FROM cursos WHERE id_curso = $1";
+  const query = "SELECT * FROM curso WHERE id_curso = $1";
   try {
     const result = await pgPool.query(query, [id_curso]);
     return result.rows[0];
@@ -36,7 +36,7 @@ async function get(id_curso) {
 }
 
 async function update(id_curso, nome_curso) {
-  const query = "UPDATE cursos SET nome_curso = $1 WHERE id_curso = $2";
+  const query = "UPDATE curso SET nome_curso = $1 WHERE id_curso = $2";
   try {
     const result = await pgPool.query(query, [nome_curso, id_curso]);
     return result.rowCount;
@@ -47,7 +47,7 @@ async function update(id_curso, nome_curso) {
 }
 
 async function remove(id_curso) {
-  const query = "DELETE FROM cursos WHERE id_curso = $1";
+  const query = "DELETE FROM curso WHERE id_curso = $1";
   try {
     await pgPool.query(query, [id_curso]);
   } catch (error) {
